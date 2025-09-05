dc82a4d5d0a28ee4eab86e26d606e40e7c5c90d4
TD <ims1228802@gmail.com>
2025-09-05 21:32:25 +0900

    first commit

---
+++ footlink/src/main/java/com/footlink/footlink/FootlinkApplication.java
@@ -0,0 +1,13 @@
+package com.footlink.footlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootlinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootlinkApplication.class, args);
+	}
+
+}
---
+++ footlink/src/test/java/com/footlink/footlink/FootlinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.footlink.footlink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootlinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
