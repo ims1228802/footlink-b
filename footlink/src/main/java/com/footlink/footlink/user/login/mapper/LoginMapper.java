4b2ddc408bfc14d4c376984bc6ed41fa78a33807
samsung <samsung@TD-NOTEBOOK>
2025-09-15 20:05:21 +0900

    .

--- footlink/src/main/java/com/footlink/footlink/user/login/mapper/LoginMapper.java
+++ footlink/src/main/java/com/footlink/footlink/user/login/mapper/LoginMapper.java
@@ -1,11 +1,11 @@
 package com.footlink.footlink.user.login.mapper;
 
+import com.footlink.footlink.user.login.DTO.LoginRequest;
 import org.apache.ibatis.annotations.Mapper;
 
-import com.footlink.footlink.user.login.DTO.LoginRequest;
 
-@Mapper
-public interface LoginMapper {
+ @Mapper
+ public interface LoginMapper {
 	
 	//회원조회
 	LoginRequest getUserInfoByEmail(String email);
