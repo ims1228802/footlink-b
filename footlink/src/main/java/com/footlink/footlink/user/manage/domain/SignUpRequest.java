510020728daf758365a75ed254cd30b4bb47e5c3
samsung <samsung@T-NOTEBOOK>
2025-09-28 15:08:20 +0900

    TD : git reset후 file 재업로드

--- footlink/src/main/java/com/footlink/footlink/user/manage/domain/SignUpRequest.java
+++ footlink/src/main/java/com/footlink/footlink/user/manage/domain/SignUpRequest.java
@@ -1,14 +1,39 @@
 package com.footlink.footlink.user.manage.domain;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
+@Data
 public class SignUpRequest {
 
 	private String id;
+	
+	@Email
+	@NotBlank
 	private String email;
+	
+	@NotBlank
+	@Size(min = 8, max = 15)
 	private String password;
+	private String confirmPassword;
+	
+	@NotBlank
+	@Pattern(regexp = "^[0-9]{11}$")
 	private String phone;
+	
 	private String birth;
 	private String gender;
-	private String addr;
+
+    private String city;
+    private String district;
+	
+	@NotBlank
 	private String name;
 	
 }
--- footlink/src/main/java/com/footlink/footlink/user/manage/service/ManageService.java
+++ footlink/src/main/java/com/footlink/footlink/user/manage/service/ManageService.java
@@ -2,9 +2,17 @@ package com.footlink.footlink.user.manage.service;
 
 import org.springframework.stereotype.Service;
 
+import com.footlink.footlink.user.manage.domain.SignUpRequest;
+
 @Service
 public interface ManageService {
 
 	boolean checkPhoneDuplicate(String phone);
+	
+    void sendVerificationCode(String phone);
+    
+    boolean verifyCode(String phone, String code);
+    
+    void signUp(SignUpRequest request);
 	
 }
