cb23e3667feae9a1569ee588b7924c8cc022680c
LeeJiseongs <137966020+LeeJiseongs@users.noreply.github.com>
2025-10-12 02:58:50 +0900

    [FL-LJS] 팀 상세 화면 수정중 detail : 팀 상세 화면 수정중

--- footlink/src/main/java/com/footlink/footlink/user/team/mapper/TeamMapper.java
+++ footlink/src/main/java/com/footlink/footlink/user/team/mapper/TeamMapper.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import com.footlink.footlink.user.myinfo.domain.MyInfo;
 import com.footlink.footlink.user.team.domain.StadiumArea;
 import com.footlink.footlink.user.team.domain.State;
 import com.footlink.footlink.user.team.domain.Team;
@@ -14,6 +15,9 @@ public interface TeamMapper {
 	List<Team> getSearchTeamList(String inputParam);
 	List<StadiumArea> getArea();
 	List<StadiumArea> getStadium();
+	Team getTeamDetail(String teamCode);
+	List<MyInfo> getTeamUserList(String teamCode);
+	State getTeamState(String teamCode);
 	void addTeamInfo(Team param);
 	void addTeamState(State param);
 }
